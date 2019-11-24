850c37617d371ea66c15a030f71a659255cddfcc
lvfei418 <56762392+lvfei418@users.noreply.github.com>
2019-11-24 10:24:03 +0800

    Update DoubleSubscriptedArray.h

--- 201816040226/Ex10_07/DoubleSubscriptedArray.h
+++ 201816040226/Ex10_07/DoubleSubscriptedArray.h
@@ -1,93 +1,33 @@
-#include <iostream>
-#include<stdexcept>
-#include<iomanip>
+#ifndef DOUBLESUBSCRIPTEDARRAY_H_INCLUDED
+#define DOUBLESUBSCRIPTEDARRAY_H_INCLUDED
+
+#include<iostream>
 #include "DoubleSubscriptedArray.h"
 using namespace std;
+class DoubleSubscriptedArray
+{
+    friend ostream &operator<<(ostream &,const DoubleSubscriptedArray &);
+    friend istream &operator>>(istream &, DoubleSubscriptedArray &);
+public:
+    DoubleSubscriptedArray(int,int);
 
-DoubleSubscriptedArray::DoubleSubscriptedArray( int arraySize)
-    :size(arraySize>0? arraySize)
-    throw invalid_argument("Array size must be great than 0")
-    ptr(new int[size])
-{
-    for(size_t i=0;i<size;+=i)
-        ptr[i]=0;
-}
-DoubleSubscriptedArray::DoubleSubscriptedArray(const DoubleSubscriptedArray &arrayToCopy)
-    :size(arrayToCopy.size)
-    ptr(new int[size])
-{
-    for(size_t i=0;i<size;+=i)
-        ptr[i]=arrayToCopy.ptr[i];
-}
-DoubleSubscriptedArray::DoubleSubscriptedArray( int r, int c)
-{
-    row=r;
-    column=c;
-    DoubleSubscriptedArray=new int[row*column];
-    for(int i=0;i<row*column;++i)
+    DoubleSubscriptedArray(const DoubleSubscriptedArray &);
+    ~DoubleSubscriptedArray();
+
+    const DoubleSubscriptedArray &operator=(const DoubleSubscriptedArray & );
+
+    bool operator==(const DoubleSubscriptedArray & )const;
+    bool operator!=(const DoubleSubscriptedArray &right)const
     {
-        *(DoubleSubscriptedArray+i)=1;
+        return ! (*this==right);
     }
-}
-DoubleSubscriptedArray::~DoubleSubscriptedArray( )
-{
-    delete [] ptr;
-}
 
-const DoubleSubscriptedArray &DoubleSubscriptedArray::operator=(const DoubleSubscriptedArray &right)
-{
-    if(&right != this )
-    {
-        if(size != right.size)
-        {
-            delete[]ptr;
-            size=right.size;
-            ptr=new int[size];
-        }
-        for(size_t i=0;i<size;++i)
-            ptr[i]=right.ptr[i];
-    }
-    return *this;
-}
-bool DoubleSubscriptedArray::operator==(const DoubleSubscriptedArray &right )const
-{
-    if(size != right.size)
-       return false;
-       for(size_t i=0;i<size;++i)
-        if(ptr[i]!=right.ptr[i])
-           return false;
-    return true;
+    int &operator()(int,int);
+    int operator()(int,int)const;
+private:
+    int *ptr;
+    int row;
+    int column;
+};
 
-}
-//overloaded subscript operator for no-const DoubleSubscriptedArry
-int &DoubleSubscriptedArray::operator()(int row,int column)
-{
-    if(row<0||row>=size||column<0||column>=size)
-        throw out_of_range("Subscript out of range");
-    return ptr(row,column);
-}
-int DoubleSubscriptedArray::operator()(int row,int column)const
-{
-    if(row<0||row>=size||column<0||column>=size)
-        throw out_of_range("Subscript out of range");
-    return ptr(row,column);
-}
-
-istream &operator>>(istream &input,DoubleSubscriptedArray &a)
-{
-    for(size_t i=0;i<a.size;++i)
-        input>>a.ptr[i];
-    return input;
-}
-ostream &operator<<(ostream &output,const DoubleSubscriptedArray &a )
-{
-    for(size_t i=0;i<a.size;++i)
-    {
-        output<<setw(12)<<a.ptr[i];
-        if((i+1)%4==0)//4 numbers per row of output
-            output << endl;
-    }
-    if(a.size%4!=0)//end last line of output
-        output<<endl;
-    return output;
-}
+#endif // DOUBLESUBSCRIPTEDARRAY_H_INCLUDED
